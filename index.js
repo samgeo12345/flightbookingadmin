fe6dc4168d059917b7ee4db748d43b8d4470bd5e
samgeo12345 <samgeoj12d@gmail.com>
2024-07-08 01:45:07 +0530

    updated

---
+++ index.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const searchcontent=document.getElementById("searchcontent");
+    const flightsearch=document.getElementById("flightsearch");
+    flightsearch.onclick=function(){
+        if(searchcontent.style.display==="none"){
+            searchcontent.style.display="block";
+            flightsearch.style.backgroundColor="dodgerblue";
+            flightsearch.style.color="white";
+        }
+        else{
+            searchcontent.style.display="none";
+            flightsearch.style.backgroundColor="";
+            flightsearch.style.color="";
+        }
+    }
+});
+document.addEventListener("DOMContentLoaded",function(){
+    const flightdetails=document.getElementById("flightdetails");
+    const detailscontent=document.getElementById("detailscontent");
+    flightdetails.onclick=function(){
+        if(detailscontent.style.display==="none"){
+            detailscontent.style.display="block";
+            flightdetails.style.backgroundColor="dodgerblue";
+            flightdetails.style.color="white";
+        }
+        else{
+            detailscontent.style.display="none";
+            flightdetails.style.backgroundColor="";
+            flightdetails.style.color="";
+        }
+    }
+});
+document.addEventListener("DOMContentLoaded",function(){
+    const addpassenger=document.getElementById("addpassenger");
+    const add=document.getElementById("add");
+    addpassenger.onclick=function(){
+        if(add.style.display==="none"){
+            add.style.display="block";
+            addpassenger.style.backgroundColor="dodgerblue";
+            addpassenger.style.color="white";
+        }
+        else{
+            add.style.display="none";
+            addpassenger.style.backgroundColor="";
+            addpassenger.style.color="";
+        }
+    }
+});
+document.addEventListener("DOMContentLoaded",function(){
+    const updatepassenger=document.getElementById("updatepassenger");
+    const update=document.getElementById("update");
+    updatepassenger.onclick=function(){
+        if(update.style.display==="none"){
+            update.style.display="block";
+            updatepassenger.style.backgroundColor="dodgerblue";
+            updatepassenger.style.color="white";
+        }
+        else{
+            update.style.display="none";
+            updatepassenger.style.backgroundColor="";
+            updatepassenger.style.color="";
+        }
+    }
+});
+document.addEventListener("DOMContentLoaded",function(){
+    const paymentoption=document.getElementById("paymentoption");
+    const payment=document.getElementById("payment");
+    paymentoption.onclick=function(){
+        if(payment.style.display==="none"){
+            payment.style.display="block";
+            paymentoption.style.backgroundColor="dodgerblue";
+            paymentoption.style.color="white";
+        }
+        else{
+            payment.style.display="none";
+            paymentoption.style.backgroundColor="";
+            paymentoption.style.color="";
+        }
+    }
+});
+document.addEventListener("DOMContentLoaded",function(){
+    const paymentoption=document.getElementById("paymentoption");
+    const payment=document.getElementById("payment");
+    paymentoption.onclick=function(){
+        if(payment.style.display==="none"){
+            payment.style.display="block";
+            paymentoption.style.backgroundColor="dodgerblue";
+            paymentoption.style.color="white";
+        }
+        else{
+            payment.style.display="none";
+            paymentoption.style.backgroundColor="";
+            paymentoption.style.color="";
+        }
+    }
+});
